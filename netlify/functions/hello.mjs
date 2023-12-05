3eec87fd968e96ac7f2cf947eb7663991ffa103a
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2023-12-05 11:52:10 -0600

    fixed function

--- netlify/functions/hello.mjs
+++ netlify/functions/hello.mjs
@@ -1,13 +1,37 @@
+// const handler = async (event, context) => {
+//   console.time("start time");
+//   const greeting = "Hello from Netlify Function!";
+//   console.timeEnd("end time");
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({
+//       message: "Hello from Netlify Function using mjs file!",
+//     }),
+//   };
+// };
+
+// export { handler };
+
 const handler = async (event, context) => {
-  console.time("start time");
-  const greeting = "Hello from Netlify Function!";
-  console.timeEnd("end time");
-  return {
+  console.time("functionExecution"); // Start measuring function execution time
+
+  // Simulate some async operation (e.g., waiting for 1 second)
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  console.timeLog("functionExecution", "Async operation completed"); // Log time after async operation
+
+  const message = "Hello from Netlify Function using mjs file!";
+
+  console.timeLog("functionExecution", "Message generated"); // Log time after message generation
+
+  const response = {
     statusCode: 200,
-    body: JSON.stringify({
-      message: "Hello from Netlify Function using mjs file!",
-    }),
+    body: JSON.stringify({ message }),
   };
+
+  console.timeEnd("functionExecution"); // End measuring function execution time and display the total time
+
+  return response;
 };
 
 export { handler };
