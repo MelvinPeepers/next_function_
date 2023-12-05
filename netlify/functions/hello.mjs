af8b739bfdaaf29176b15db9904ab6cf5a44d4a6
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2023-12-05 12:08:58 -0600

    fixed function

--- netlify/functions/hello.mjs
+++ netlify/functions/hello.mjs
@@ -1,16 +1,4 @@
-// const handler = async (event, context) => {
-//   console.time("start time");
-//   const greeting = "Hello from Netlify Function!";
-//   console.timeEnd("end time");
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify({
-//       message: "Hello from Netlify Function using mjs file!",
-//     }),
-//   };
-// };
-
-// export { handler };
+// https://answers.netlify.com/t/support-guide-why-is-my-function-taking-long-or-timing-out/71689
 
 const handler = async (event, context) => {
   console.time("functionExecution"); // Start measuring function execution time
