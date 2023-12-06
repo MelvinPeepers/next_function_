225aa3bd3f2fc231ccc5eb6a6a15999842813d5d
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2023-12-06 09:29:38 -0600

    scheduled function test

---
+++ netlify/functions/test-scheduled-function.mjs
@@ -0,0 +1,10 @@
+const handler = async (event, context) => {
+  console.log("Scheduled function ran at: ", new Date().toISOString());
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Scheduled function ran successfully" }),
+  };
+};
+
+export { handler };
